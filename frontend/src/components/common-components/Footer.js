a57fd02c2297d388900210a14474c67fdc8f4c9d
ujstha <ujstha27@gmail.com>
2020-05-24 19:18:29 +0300

    (develop) - styled Error 404 page

--- frontend/src/components/common-components/Footer.js
+++ frontend/src/components/common-components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { navs } from '../../constants/navItems';
 
 const Footer = () => {
   return (
     <div className='footer'>
-      TMDbCine &copy; {new Date().getFullYear()}. All rights reserved.
+      <span>
+        <ul>
+          {navs
+            .filter((nav) => nav.type === 'both')
+            .map((nav, index) => (
+              <li key={index} className='nav-link'>
+                <a href={nav.path}>{nav.title}</a>
+              </li>
+            ))}
+        </ul>
+      </span>
+      <hr />
+      <a href='/'>TMDbCine</a> &copy; {new Date().getFullYear()}. All rights
+      reserved.
     </div>
   );
 };
--- frontend/src/components/helper-components/ErrorPage.js
+++ frontend/src/components/helper-components/ErrorPage.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Empty } from 'antd';
+import { Empty, Button } from 'antd';
+import errorImg from '../../assets/images/404.png';
 
 export const ErrorPage = () => {
   return (
-    <div className='error-page'>
-      <h1>Error Page</h1>
+    <div className='error-page-container'>
+      <div className='error-page'>
+        <div className='error-img'>
+          <img src={errorImg} alt='error img' />
+        </div>
+        <div className='error-details'>
+          <h1>4&#9785;4</h1>
+          <p>Page Not Found</p>
+          <span className='error-msg'>
+            Sorry, the page you are looking for does not exists.
+          </span>
+          <Button type='primary' href='/'>
+            &ensp;Back to Home
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
