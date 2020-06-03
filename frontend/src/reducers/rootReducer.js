b2f052b088455b8e555cc289d495fd1be35c05bc
ujstha <ujstha27@gmail.com>
2020-06-03 20:53:14 +0300

    (refactor) - imported and combined reducers in root reducer

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,6 +1,19 @@
 import { combineReducers } from 'redux';
+import { genresReducer } from './genresReducer';
+import { personReducer } from './personsReducer';
+import { moviesReducer, movieReducer } from './moviesReducer';
+import { tvsReducer, tvReducer, tvSeasonReducer } from './tvsReducer';
+import { searchReducer } from './searchReducer';
 import { homeReducer } from './homeReducer';
 
 export const rootReducer = combineReducers({
   home: homeReducer,
+  genres: genresReducer,
+  person: personReducer,
+  movies: moviesReducer,
+  movie: movieReducer,
+  tvs: tvsReducer,
+  tv: tvReducer,
+  tvSeason: tvSeasonReducer,
+  searchResults: searchReducer,
 });
