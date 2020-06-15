5add556ff18863476373ac782bb182ae73dec5a5
ujstha <ujstha27@gmail.com>
2020-06-15 15:06:24 +0300

    (refactor) - imported user and media reducer in root reducer

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -4,9 +4,15 @@ import { personReducer } from './personsReducer';
 import { moviesReducer, movieReducer } from './moviesReducer';
 import { tvsReducer, tvReducer, tvSeasonReducer } from './tvsReducer';
 import { searchReducer } from './searchReducer';
+import { registerReducer, loginReducer } from './usersReducer';
+import { saveMediaReducer, fetchMediaReducer } from './mediasReducer';
 import { homeReducer } from './homeReducer';
 
 export const rootReducer = combineReducers({
+  register: registerReducer,
+  login: loginReducer,
+  saveMedia: saveMediaReducer,
+  media: fetchMediaReducer,
   home: homeReducer,
   genres: genresReducer,
   person: personReducer,
